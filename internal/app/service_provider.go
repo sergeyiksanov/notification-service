fea3238bfc4ba7f5543809c756c2780da0927905
sergeyiksanov <sergeyiksanov25@gmail.com>
2024-10-30 01:22:04 +0300

    update service provider

---
+++ internal/app/service_provider.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"NotificationService/internal/config"
+	"log"
+)
+
+type serviceProvider struct {
+	rabbitMqConfig *config.RabbitMqConfig
+}
+
+func newServiceProvider() *serviceProvider {
+	return &serviceProvider{}
+}
+
+func (s *serviceProvider) getRabbitMqProvider() *config.RabbitMqConfig {
+	if s.rabbitMqConfig == nil {
+		r, err := config.NewRabbitMqConfig()
+
+		if err != nil {
+			log.Fatalf("Failed set rabbit mq config: %s", err)
+		}
+
+		s.rabbitMqConfig = r
+	}
+
+	return s.rabbitMqConfig
+}
